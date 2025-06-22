2f22cb995929b3e58508d0574ce120a3f72d052d
Daniel Deaconescu <daniel.deaconescu23@gmail.com>
2025-06-22 05:37:32 +0300

    added the logo and other stylings

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,14 @@
+import styled from "styled-components";
+import LogoImg from "../assets/images/read-n-rated-logo-white.png";
+
+const StyledImg = styled.img`
+  max-width: 60px;
+`;
+
 function Logo() {
   return (
     <div className="logo">
+      <StyledImg src={LogoImg} alt="Read'n Rated logo" />
       <h1>Read'n Rated</h1>
     </div>
   );
